c5aa94697c4d6de90ea5057630cddf24acdfb39f
Guilherme Felix <f3lixguilherme@gmail.com>
2025-03-24 18:33:25 -0300

    teste

--- projetos-teoricos/estudos/src/JavaNiO/SistemaDeCadastro.java
+++ projetos-teoricos/estudos/src/JavaNiO/SistemaDeCadastro.java
@@ -1,12 +1,11 @@
 package JavaNiO;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class SistemaDeCadastro {
     public static void main(String[] args) {
-        List <Cadastro> cadastros = new ArrayList<>();
 
+        System.out.println("teste");
 
     }
 }
